d0656a7ba50f03176643dc1a593c42e56fd67e4c
Zesky665 <zharkoc@gmail.com>
2023-05-16 14:31:49 +0200

    Update for striki

--- utils/checkpoints.py
+++ utils/checkpoints.py
@@ -45,7 +45,6 @@ def save_weights(model, args, epoch, optimizer):
 
     model_name = '{}_{}_{}'.format(args.base_model_name, epoch, args.lr)
     torch.save(state, '{}/{}.pt'.format(args.model_path, model_name))
-    model.save(f'model/my_model-{model_name}')
     return model_name
 
 def load_weights(model, args):
